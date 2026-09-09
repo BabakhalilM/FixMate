06a1c29da36411ea4911badc709af4eacfb3a340
BabakhalilM <babakhalilmalyam2@gmail.com>
2026-09-09 19:35:18 +0530

    cors landingwebsite added

--- apps/api-server/src/index.ts
+++ apps/api-server/src/index.ts
@@ -15,7 +15,8 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT ? Number(process.env.PORT) : 5000;
 
-app.use(cors());
+app.use(cors({ origin: 'https://landing-website-orpin.vercel.app', }));
+
 // app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
